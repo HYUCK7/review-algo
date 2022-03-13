50a7719518a646dd22168169989d3dee212e1b9c
HYUCK7 <iqeq12191219@gmail.com>
2022-03-14 00:07:12 +0900

    2022-03-14 12:07 주말 퀴즈 최종

--- algo/quiz30.py
+++ algo/quiz30.py
@@ -9,11 +9,6 @@ from algo.domain import myRandom
 
 class Quiz30:
     def quiz30_df_4_by_3(self):
-        df = pd.DataFrame([[1, 2, 3],
-                           [4, 5, 6],
-                           [7, 8, 9],
-                           [10, 11, 12]], index=range(1, 5), columns=['A', 'B', 'C'])
-        # 위 식을 리스트 결합 형태로 분해해서 조립하시오
         # (1)
         a = []
         b = []
@@ -32,14 +27,6 @@ class Quiz30:
         ic(df1)
         # j = [1,2,3] * 3 + 1
 
-    '''
-        데이터프레임 문제 Q03.
-        두자리 정수를 랜덤으로 2행 3열 데이터프레임을 생성
-        ic| df:     0   1   2
-                0  97  57  52
-                1  56  83  80
-    '''
-
     def quiz31_rand_2_by_3(self):
         c = [i for i in range(0, 3)]  # 0 , 1,  2
         # (1)
@@ -57,37 +44,18 @@ class Quiz30:
         df1 = pd.DataFrame(e, index=range(0, 2), columns=c)
         ic(df1)
 
-    '''
-            데이터프레임 문제 Q04.
-            국어, 영어, 수학, 사회 4과목을 시험치른 10명의 학생들의 성적표 작성.
-             단 점수 0 ~ 100이고 학생은 랜덤 알파벳 5자리 ID 로 표기
-
-              ic| df4:        국어  영어  수학  사회
-                        lDZid  57  90  55  24
-                        Rnvtg  12  66  43  11
-                        ljfJt  80  33  89  10
-                        ZJaje  31  28  37  34
-                        OnhcI  15  28  89  19
-                        claDN  69  41  66  74
-                        LYawb  65  16  13  20
-                        QDBCw  44  32   8  29
-                        PZOTP  94  78  79  96
-                        GOJKU  62  17  75  49
-    '''
-
-    def quiz32_df_grade(self, j=None):
-        ''' id_make = [[''.join(string.ascii_uppercase)[myRandom(0, 26)] for i in range(5)] for i in range(10)]
-        # sub = ['국어', '영어', '수학', '사회']
-        # grade = [[myRandom(0, 100) for i in range(4)] for j in range(10)]
-        # df2 = pd.DataFrame(grade, index=id_make, columns=sub)
-        # ic(df2)'''
+    def quiz32_df_grade(self):
         sub = ['kor', 'eng', 'math', 'soc']
         arr2 = [''.join([string.ascii_letters[myRandom(0, 52)] for i in range(5)]) for i in range(10)]
         grade = [[myRandom(0, 100) for i in range(4)] for j in range(10)]
-        dict = {arr2[0]: grade}
-        df = pd.DataFrame.from_dict(dict, orient='index', columns=sub)
+        df = pd.DataFrame(grade, index=arr2, columns=sub)
         ic(df)
 
+        ''' id_make = [[''.join(string.ascii_uppercase)[myRandom(0, 26)] for i in range(5)] for i in range(10)]
+                # sub = ['국어', '영어', '수학', '사회']
+                # grade = [[myRandom(0, 100) for i in range(4)] for j in range(10)]
+                # df2 = pd.DataFrame(grade, index=id_make, columns=sub)
+                # ic(df2)'''
         '''id_make = random.()string.ascii_uppercase
         #print(id_make)
         #id = random.choice(id_make)
